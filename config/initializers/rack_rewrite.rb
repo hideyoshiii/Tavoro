93d68beedc06bbc189e417b55cc4404f640b942d
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-10-22 09:58:32 +0900

    fix36

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,5 +1,5 @@
 if ENV['RACK_ENV'] == 'production'
-  Powder::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+  Tavore::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
     r301 %r{.*}, 'https://www.tavore.net$&', :if => Proc.new {|rack_env|
       rack_env['SERVER_NAME'] == 'tavore.herokuapp.com'
     }
