37fb79b33e27667bfc34b9693c46e01be7796318
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-10-27 10:12:35 +0900

    fix73

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -21,7 +21,7 @@ class WorksController < ApplicationController
           end
           #@postsを新しい順に並べたい
           @posts.sort_by!{|post| post.created_at}.reverse!
-          @posts = @posts.take(10)
+          @posts = @posts.take(30)
       end
     end
   end
