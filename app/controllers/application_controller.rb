fa8a4beb803c5c405709d8af9ff0978e45e440e8
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-11-03 18:16:28 +0900

    fix104

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  
-
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
   rescue_from Exception, with: :render_500
