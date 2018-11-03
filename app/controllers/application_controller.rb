17acc2b758f141ebb1e0e4631ef27e9438fb09de
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2018-11-03 15:50:29 +0900

    fix103

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,31 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
+
+  def render_404(exception = nil)
+    if exception
+      logger.info "Rendering 404 with exception: #{exception.message}"
+    end
+    render template: "errors/error_404", status: 404, layout: 'application'
+  end
+
+  def render_500(exception = nil)
+    if exception
+      logger.info "Rendering 500 with exception: #{exception.message}"
+    end
+    render template: "errors/error_500", status: 500, layout: 'application'
+  end
+
+
+
+
+
   def after_sign_in_path_for(resource)
         root_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -63,4 +63,7 @@ Rails.application.routes.draw do
 
   post "likes/:post_id/create" => "likes#create"
   post "likes/:post_id/destroy" => "likes#destroy"
+
+
+  get '*path', to: 'application#render_404'
 end
