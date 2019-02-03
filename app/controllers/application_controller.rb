6149da27921e55210061ddfb02b9b27ca9d47e81
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2019-02-04 08:12:19 +0900

    fix369

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,11 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-        root_path
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    root_path
   end
 
   private
